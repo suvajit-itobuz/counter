bec1f8e9a5392252374412c2e66f2c0a0ec7ff65
Suvajit Saha <suvajit@itobuz.com>
2024-12-26 18:30:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const counterIncrease = document.getElementById("counter-increase");
+const counterDecrease = document.getElementById("counter-decrease");
+const counterReset = document.getElementById("counter-reset");
+const counterNum = document.getElementById("counter-num");
+let value = counterNum.innerText;
+console.log(value);
+const check = (value) => {
+  if (value > 0) {
+    counterNum.style.color = "green";
+  } else if (value < 0) {
+    counterNum.style.color = "red";
+  } else counterNum.style.color = "black";
+};
+const increase = () => {
+  value = Number(value) + 1;
+  counterNum.innerText = value;
+  check(value);
+};
+const decrease = () => {
+  value = Number(value) - 1;
+  counterNum.innerText = value;
+  check(value);
+};
+const reset = () => {
+  value = 0;
+  counterNum.innerText = value;
+  check(value);
+};
